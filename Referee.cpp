9b35133966e2bca3b264161171f931788d673872
QinBaosen <90596147+QinBaosen@users.noreply.github.com>
2022-08-06 23:26:47 +0930

    Add files via upload

--- Referee.cpp
+++ Referee.cpp
@@ -2,17 +2,17 @@
 
 char Referee::refGame(Human player1, Computer player2){
     char playerChoice = player1.makeMove();
-    char compChoice = 'R';
+    char compChoice = player2.makeMove();
 
     // Decide which of the two players wins
     // L = human loses; W = human wins; T = tie
     if(playerChoice == compChoice){
         return 'T';
-    }else if(playerChoice == 'R' && compChoice == 'S'){
+    }else if(playerChoice == 'P' && compChoice == 'R'){
         return 'W';
     }else if(playerChoice == 'S' && compChoice == 'P'){
         return 'W';
-    }else if(playerChoice == 'P' && compChoice == 'R'){
+    }else if(playerChoice == 'R' && compChoice == 'S'){
         return 'W';
     }else{
         return 'L';
