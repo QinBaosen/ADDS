7ef991d81bc4d271c55e3ced203e976614725e47
QinBaosen <90596147+QinBaosen@users.noreply.github.com>
2022-08-26 14:39:25 +0930

    Add files via upload

--- EfficientTruckloads.cpp
+++ EfficientTruckloads.cpp
@@ -1,11 +1,22 @@
 #include "EfficientTruckloads.h"
-using namespace std;
+
 #include <iostream>
+#include <vector>
+using std::vector;
 
+EfficientTruckloads::EfficientTruckloads(){
 
-
-int EfficientTruckloads::numTrucks(int numCrates, int loadSize){
-
+}
+int EfficientTruckloads :: numTrucks(int numCrates, int loadSize){
+    // if((numCrates>10000)||(numCrates<2)){
+    //     cout << "ERROR";
+    //     return 0;
+    // }
+    // if((loadSize>(numCrates-1))||(loadSize<1)){
+    //     cout << "ERROR";
+    //     return 0;
+    // }
+    static vector <int> truckTable(10000, -1);
     if((numCrates <= loadSize) && (numCrates >0)){
         return 1;
     }
@@ -13,6 +24,8 @@ int EfficientTruckloads::numTrucks(int numCrates, int loadSize){
     if(numCrates < 1){
         return 0;
     }
+    truckTable.at(numCrates) = numTrucks(numCrates/2 + numCrates%2, loadSize) +numTrucks(numCrates/2, loadSize);
+    return truckTable.at(numCrates);
 
-    return numTrucks(numCrates/2,loadSize)+numTrucks(numCrates/2,loadSize);
+
 }
--- Reverse.cpp
+++ Reverse.cpp
@@ -4,32 +4,45 @@ using namespace std;
 #include <string>
 #include <cmath>
 #include <iostream>
+#include <stdio.h>
 
-int lastdigit =0;
 int Reverse::reverseDigit(int value){
-    int apow = to_string(value).length();
+
     if(value < 0 ){
 
         return -1;
     }
+    if(value = 0 ){
 
-    if(value <=10){
-        return value+ lastdigit;
+        return 0;
     }
-   int digit = (int) log10(value);
 
-
-
-   return ((value%10 * pow(10, digit)) + reverseDigit(value/10));
+    int iResault = 0;
+	static int iPos = 10;
+	if(value/10 == 0)
+	{
+		iPos = 10;
+		return value;
+	}
+	iResault = reverseDigit(value/10) + value % 10 * iPos;
+	iPos *= 10;
+	return iResault;
 
 }
 
+
 string Reverse::reverseString(string letters){
-	string word = "";
-	if(letters.length() < 0)
+	int len=letters.length();
+
+	if(len <= 0)
     {
         return "ERROR";
+
+    }else if(len == 1){
+        return letters;
     }
+
+	return reverseString(letters.substr( 1))+letters.substr(0,1);
 
 
 }
