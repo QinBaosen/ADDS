f06566a89c6520bf573e01abec7f473d9f52a6ad
QinBaosen <90596147+QinBaosen@users.noreply.github.com>
2022-08-26 13:49:32 +0930

    Add files via upload

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -3,14 +3,7 @@ using namespace std;
 #include <iostream>
 
 int Truckloads::numTrucks(int numCrates, int loadSize){
-    if((numCrates>10000)||(numCrates<2)){
-        cout << "ERROR";
-        return 0;
-    }
-    if((loadSize>(numCrates-1))||(loadSize<1)){
-        cout << "ERROR";
-        return 0;
-    }
+    
     if((numCrates <= loadSize) && (numCrates >0)){
         return 1;
     }
