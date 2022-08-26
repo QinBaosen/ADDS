aa572c9f5798c06457031ca7af3572f5f86590e4
QinBaosen <90596147+QinBaosen@users.noreply.github.com>
2022-08-26 13:48:17 +0930

    Add files via upload

--- EfficientTruckloads.cpp
+++ EfficientTruckloads.cpp
@@ -1,11 +1,11 @@
 #include "EfficientTruckloads.h"
 using namespace std;
 #include <iostream>
-#include <vector>
-using std::vector;
 
-static int* p = new int[10000];
+
+
 int EfficientTruckloads::numTrucks(int numCrates, int loadSize){
+
     if((numCrates <= loadSize) && (numCrates >0)){
         return 1;
     }
@@ -13,8 +13,6 @@ int EfficientTruckloads::numTrucks(int numCrates, int loadSize){
     if(numCrates < 1){
         return 0;
     }
-    p[numCrates] = numTrucks(numCrates/2, loadSize) +numTrucks(numCrates/2 - numCrates/2, loadSize);
-    return p[numCrates];
 
-
+    return numTrucks(numCrates/2,loadSize)+numTrucks(numCrates/2,loadSize);
 }
