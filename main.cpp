717f9574fbf58324edc48eec006a35acf2e1dc10
Connor <90596147+QinBaosen@users.noreply.github.com>
2022-09-05 13:06:53 +0930

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "FilterForTwoDigitPositive.h"
+#include "FilterNonPositive.h"
+#include "FilterOdd.h"
+#include "FliterGeneric.h"
+#include "ReduceMinimum.h"
+#include "ReduceGeneric.h"
+#include "ReduceGCD.h"
+#include "MapGeneric.h"
+#include "MapTriple.h"
+#include "MapSquare.h"
+#include "MapAbsoluteValue.h"
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
