c9d5d1a9e2f431a40846a3aa6d58478ae0d1e44a
Connor <90596147+Connor-Qin@users.noreply.github.com>
2022-10-20 00:11:39 +1030

    Add files via upload

--- Polish_prefix.cpp
+++ Polish_prefix.cpp
@@ -1,3 +1,115 @@
+#include "Polish_prefix.h"
 #include <iostream>
+#include <string>
 using namespace std;
+int Polish_prefix::opCal(int a, int b, char c) {
+    switch (c) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
+    }
+    
+    return 0;
+}
+int Polish_prefix::number(){
+    if (opd.size() == 1) {
+ 		cout << function << "= " << opd.top() << endl;
+ 	}
+	else {
+ 		cout << "wrong operand" << endl;
+ 		return 0;
+ 	}
+    return 1;
+}
 
+int Polish_prefix::Install(string x){
+    int len = temp.size();
+    	for (int i = 0; i < len; i++) {
+		double num = temp.top();
+		opd.push(num);
+		temp.pop();
+	}
+
+	if (opd.empty() || (opts.size() >= opd.size()) || ((opd.size() - 1) != opts.size() )){
+		cout << "Error" << endl;
+		return 0;
+	}
+    for (int i = 0; i < x.length(); i++) {
+
+		if (x.at(i) == '+' || x.at(i) == '*' || x.at(i) == '-' || x.at(i) == '/' ) {
+			opts.push(x.at(i));
+		}else if (isdigit(x.at(i)) == true) {
+			double num = x.at(i) - '0';
+			temp.push(num);
+
+			for (int j = i; j < x.length(); j++) {
+
+				if (x.at(j) == '-' || x.at(j) == '+' || x.at(j) == '*' || x.at(j) == '/' ) {
+					cout << "Error" << endl;
+					return 0;
+				}
+			}
+		}
+	}
+
+    return 1;
+}
+void Polish_prefix::Calculator(){
+
+    int count =0;
+	while (opd.size() > 1) {
+
+		count++;
+
+		num1 = opd.top();
+		if (count == 1) {
+
+			function = function +to_string(num1) + " ";
+		}
+
+		opd.pop();
+		op = opts.top();
+
+		if ((op == '*' || '/') && count == 1) {
+			function = "(" + function + op + " ";
+		}
+		else if ((op == '*' || '/')){
+			function = function + op + " ";
+		}
+		else {
+			function + op;
+		}
+
+ 		opts.pop();
+
+		num2 = opd.top();
+		if ((op == '*' || '/')  && count == 1) {
+			function = function +  to_string(num2) + ") ";
+		}
+		else {
+			function = function + to_string(num2);
+		}
+
+ 		opd.pop();
+
+ 		if (op == '+') {
+ 			result = num1 + num2;
+ 		}
+		else if (op == '-') {
+ 			result = num1 - num2;
+ 		}
+		else if (op == '*') {
+ 			result = num1 * num2;
+ 		}
+		else if (op == '/') {
+ 			result = num1 / num2;
+ 		}
+ 		opd.push(result);
+
+ 	}
+}
--- main.cpp
+++ main.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+using namespace std;
+#include <stdlib.h>
+#include <string>
+#include "Polish_prefix.cpp"
+/*
+Sample input:  * - 5 6 7
+Sample output:  (5 - 6) * 7 = -7
 
+Sample input: * 5
+Sample output: Error
+
+Sample input: * 5 6 7
+Sample output: Error
+*/
+
+
+int main() {
+
+	string str;
+	getline(cin,str);
+	if (str.length() == 0 || str.length() == 1) {
+		cout << "Error" << endl;
+		return 0;
+	}
+
+	if (str.at(0) != '+' && str.at(0) != '-' && str.at(0) != '*' && str.at(0) != '/' ) {
+		cout << "Error" << endl;
+		return 0;
+	}
+
+	Polish_prefix pp;
+	int error;
+    error = pp.Install(str);
+    if (error == 0)
+    {
+        return 0;
+    }
+    pp.Calculator();
+    error = pp.number();
+    if (error == 0)
+    {
+        return 0;
+    }
+
+
+}
